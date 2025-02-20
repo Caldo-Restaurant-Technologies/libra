db0e222cc9497ab76204559ff7fc9987c13f3095
Jorge Guerra <j.a.guerra@protonmail.com>
2025-02-19 16:34:27 -0800

    first commit

---
+++ src/scale.rs
@@ -0,0 +1,63 @@
+use phidget::ReturnCode;
+use phidget::{devices::VoltageRatioInput, Phidget};
+use std::{array, time::Duration};
+const NUMBER_OF_INPUTS: usize = 4;
+const TIMEOUT: Duration = phidget::TIMEOUT_DEFAULT;
+
+fn dot_product(a: &[f64], b: &[f64]) -> f64 {
+    a.iter().zip(b.iter()).map(|(a, b)| a * b).sum::<f64>()
+}
+
+pub enum Error {
+    InvalidCoefficients,
+    InvalidPhidgetId,
+    PhidgetError,
+}
+pub struct Scale {
+    phidget_id: i32,
+    vins: [VoltageRatioInput; NUMBER_OF_INPUTS],
+}
+
+impl Scale {
+    pub fn new(phidget_id: i32) -> Self {
+        Self {
+            phidget_id,
+            vins: array::from_fn(|_| VoltageRatioInput::new()),
+        }
+    }
+
+    pub fn connect(mut self) -> Self {
+        self.vins.iter_mut().enumerate().for_each(|(i, vin)| {
+            let min_interval = vin
+                .min_data_interval()
+                .expect("Unable to get min data interval");
+
+            vin.set_serial_number(self.phidget_id)
+                .expect("Invalid phidget_id");
+
+            vin.set_channel(i as i32).expect("Invalid Channel");
+
+            vin.open_wait(TIMEOUT)
+                .expect("Failed to open Phidget connection");
+
+            vin.set_data_interval(min_interval)
+                .expect("Failed to set data interval");
+        });
+        self
+    }
+
+    pub fn get_weight(&self, coefficients: &[f64], offset: f64) -> Result<f64, Error> {
+        if coefficients.len() != self.vins.len() {
+            return Err(Error::InvalidCoefficients);
+        }
+        let readings = self.get_raw_readings();
+        match readings {
+            Ok(readings) => Ok(dot_product(readings.as_slice(), coefficients) - offset),
+            Err(_) => Err(Error::PhidgetError),
+        }
+    }
+
+    fn get_raw_readings(&self) -> Result<Vec<f64>, ReturnCode> {
+        self.vins.iter().map(|vin| vin.voltage_ratio()).collect()
+    }
+}
