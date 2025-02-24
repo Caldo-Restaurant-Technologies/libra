62671ac38dd32c0584fead4d93fab8f851151f1e
Jorge Guerra <j.a.guerra@protonmail.com>
2025-02-24 12:58:39 -0800

    Refactored scale into typestates

--- src/scale.rs
+++ src/scale.rs
@@ -1,8 +1,9 @@
 use phidget::ReturnCode;
 use phidget::{devices::VoltageRatioInput, Phidget};
+use std::fmt;
 use std::{array, time::Duration};
 const NUMBER_OF_INPUTS: usize = 4;
-const TIMEOUT: Duration = phidget::TIMEOUT_DEFAULT;
+pub const TIMEOUT: Duration = phidget::TIMEOUT_DEFAULT;
 
 fn dot_product(a: &[f64], b: &[f64]) -> f64 {
     a.iter().zip(b.iter()).map(|(a, b)| a * b).sum::<f64>()
@@ -14,41 +15,92 @@ pub enum Error {
     InvalidPhidgetId,
     PhidgetError,
 }
-pub struct Scale {
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::InvalidCoefficients => write!(f, "Invalid coefficients"),
+            Error::InvalidPhidgetId => write!(f, "Invalid Phidget ID"),
+            Error::PhidgetError => write!(f, "Phidget error"),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+        Error::PhidgetError
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(_: std::num::ParseIntError) -> Self {
+        Error::InvalidCoefficients
+    }
+}
+
+impl From<std::string::ParseError> for Error {
+    fn from(_: std::string::ParseError) -> Self {
+        Error::InvalidPhidgetId
+    }
+}
+
+pub struct DisconnectedScale {
+    phidget_id: i32,
+}
+
+impl DisconnectedScale {
+    pub fn new(phidget_id: i32) -> Self {
+        Self { phidget_id }
+    }
+
+    pub fn connect(
+        self,
+        offset: f64,
+        coefficients: [f64; NUMBER_OF_INPUTS],
+        timeout: Duration,
+    ) -> Result<ConnectedScale, Error> {
+        let vins = array::from_fn(|i| {
+            let mut vin = VoltageRatioInput::new();
+            vin.set_serial_number(self.phidget_id)
+                .expect("Invalid Phidget Serial Number");
+            vin.set_channel(i as i32).unwrap(); //This is ok because its impossible for i to exceed
+                                                //the number of channels
+            vin.open_wait(timeout)
+                .expect("Unable to connect to phidget");
+            let min_interval = vin.min_data_interval().expect("Unable to get min interval");
+            vin.set_data_interval(min_interval)
+                .expect("Unable to set data interval");
+            vin
+        });
+        Ok(ConnectedScale::new(
+            self.phidget_id,
+            offset,
+            coefficients,
+            vins,
+        ))
+    }
+}
+
+pub struct ConnectedScale {
     phidget_id: i32,
     offset: f64,
-    coefficients: [f64; 4],
+    coefficients: [f64; NUMBER_OF_INPUTS],
     vins: [VoltageRatioInput; NUMBER_OF_INPUTS],
 }
 
-impl Scale {
-    pub fn new(phidget_id: i32, offset: f64, coefficients: [f64; 4]) -> Self {
+impl ConnectedScale {
+    fn new(
+        phidget_id: i32,
+        offset: f64,
+        coefficients: [f64; NUMBER_OF_INPUTS],
+        vins: [VoltageRatioInput; NUMBER_OF_INPUTS],
+    ) -> Self {
         Self {
             phidget_id,
             offset,
             coefficients,
-            vins: array::from_fn(|_| VoltageRatioInput::new()),
+            vins,
         }
-    }
-
-    pub fn connect(mut self) -> Self {
-        self.vins.iter_mut().enumerate().for_each(|(i, vin)| {
-            vin.set_serial_number(self.phidget_id)
-                .expect("Invalid phidget_id");
-
-            vin.set_channel(i as i32).expect("Invalid Channel");
-
-            vin.open_wait(TIMEOUT)
-                .expect("Failed to open Phidget connection");
-
-            let min_interval = vin
-                .min_data_interval()
-                .expect("Unable to get min data interval");
-
-            vin.set_data_interval(min_interval)
-                .expect("Failed to set data interval");
-        });
-        self
     }
 
     pub fn update_coefficients(self, coefficients: [f64; 4]) -> Self {
